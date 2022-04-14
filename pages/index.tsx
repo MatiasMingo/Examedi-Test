8a651243a834e063fb3d57905511c95a0e6099a2
Matías Mingo <mamingo@uc.cl>
2022-04-13 20:27:44 -0400

    feat:deploy to heroku

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = (props) => {
         </div>
         <div className="grid grid-cols-4 gap-4">
           {pokemons?.map((pokemon, i) => (         
-              <Card pokemon={pokemon} id={i}/>
+              <Card pokemon={pokemon} key={i}/>
               )
             )
           }
